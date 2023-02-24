6c03e1ea679f19d9c5be58dfd4239e6c078dd6c7
Eddie <educatedsavage@educatedsavage.com>
2023-02-23 18:38:18 -0800

    added responsive menu for mobile

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+unhide();
+
+function unhide() {
+      var x = document.getElementById("mobile-nav");
+      if (x.style.display === "none") {
+            x.style.display = "block";
+      } else {
+            x.style.display = "none";
+      }
+  }
