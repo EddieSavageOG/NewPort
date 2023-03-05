f5c95bceb9ca5f86b0682ca54f03fe0078eaf737
Eddie <educatedsavage@educatedsavage.com>
2023-03-05 14:20:57 -0800

    transitioning to PHP with components

---
+++ assets/inc/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <p>© 2023 - Copyright by Charlene Slimp</p>
+</footer>
+
+<script src="assets/js/main.js"></script>
+<script src="slider.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE HTML>
-
 <!-- 
 
 TODO: figure out the favicon
@@ -13,56 +11,9 @@ TODO: More interest in footers?
 -->
 
 
-<html lang="en">
-<head>
-  	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NR8V8GS');
-    </script>
-    <!-- End Google Tag Manager -->
-  <title>Charlene Slimp - Software Engineer</title>
-	<meta charset="utf-8" />
-  <!-- TODO: update this below -->
-	<meta name="viewport" content="width=device-width, initial-scale=1" />        
-	<link rel="icon" type="image/x-icon" href="/images/favicon.ico"> 
-	<link rel="stylesheet" href="styles.css" />
-	<link href="fcf-assets/css/fcf.default.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-</head>
-<body>
-  	<!-- Google Tag Manager (noscript) -->
-	<noscript>
-		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NR8V8GS"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe>
-	</noscript>
-	<!-- End Google Tag Manager (noscript) -->
-<header>
-  <nav>
-    <ul class = "desktop-nav">
-      <li><a href = "index.html">Home</a></li>
-      <!-- <li><a href = "./blogdex">Blog</a></li> -->
-      <li><a href = "index.html#port1">Website Portoflio</a></li>
-      <li><a href = "index.html#port2">JavaScript and Python Portfolio</a></li>
-      <li><a href = "about.html">About</a></li>
-      <li><a href = "contact.html">Contact</a></li>
-    </ul>
-    <div class = "hamburger-nav">
-      <a href = "javascript:void(0);" class="hamburger" onClick="unhide()"><img src = "assets/images/hamburger.png" alt="access mobile menu"></a>
-      <ul id="mobile-nav">
-        <li><a href = "index.html">Home</a></li>
-        <!-- <li><a href = "./blogdex">Blog</a></li> -->
-        <li><a href = "index.html#port1">Website Portoflio</a></li>
-        <li><a href = "index.html#port2">JavaScript and Python Portfolio</a></li>
-        <li><a href = "about.html">About</a></li>
-        <li><a href = "contact.html">Contact</a></li>
-      </ul>
+<?php include './assets/inc/header.php'; ?>
 
 
-  </nav>
-</header>
 <main>
   <h1 class="name"><span class="hilight">Charlene Slimp</span></h1>
   <h1 class="name">Software Engineer</h1>
@@ -169,12 +120,6 @@ TODO: More interest in footers?
     </ol>
   </section>
 </main> 
-<footer>
-  <p>© 2023 - Copyright by Charlene Slimp</p>
-</footer>
 
-<script src="assets/js/main.js"></script>
-<script src="slider.js"></script>
 
-</body>
-</html>
+<?php include './assets/inc/footer.php'; ?>
